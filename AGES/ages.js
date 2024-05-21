4e2ab4b57ae2860ee3133f91a0153c9ed2d0af24
Diego <joan_chinchay@hotmail.com>
2024-05-21 17:43:16 -0500

    AGES/

---
+++ AGES/ages.js
@@ -0,0 +1,7 @@
+    const edad = Number (prompt('ingrese su edad porfavor:'));
+    if (edad>18){alert('mayor de edad')
+    }
+    else if (edad>0)
+    {alert('menor de edad')}
+    
+else {alert('error de usuario')}
---
+++ interacciones/interaciones.js
@@ -0,0 +1,5 @@
+const interaciones =  prompt ('DESEAS SABER LA SUMA DE TU AÑO DE NACIMIENTO,?');
+let suma = 0;
+for(let i= 1; i<=interaciones; i++)
+     { suma = suma + i }
+alert (' LA SUMA DE LA EDAD ES:'+ suma)
